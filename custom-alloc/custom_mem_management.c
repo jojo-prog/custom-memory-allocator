7dc35d297fc62fd58279263bb2406f3fab21d92a
jojo-prog <johannes16.braeuer@gmail.com>
2025-01-18 17:44:16 +0100

    feat: a custom free function
    merges adjacent free blocks

--- custom-alloc/custom_mem_management.c
+++ custom-alloc/custom_mem_management.c
@@ -3,19 +3,83 @@
 
 meta_data merge_blocks(meta_data block)
 {
-   
+  if (block && block->next && block->next->free)
+  {
+    block->size += META_DATA_SIZE + block->next->size;
+    block->next = block->next->next;
+  }
+  if (block->next)
+  {
+    block->next->prev = block;
+  }
+  return block;
+}
+
+meta_data get_block_addr(void *p)
+{
+  return (meta_data)p - 1;
+}
+
+int is_valid_addr(void *p)
+{
+  if (heap_head)
+  {
+    if (p > heap_head && p < sbrk(0))
+    {
+      void *ptr = (get_block_addr(p))->ptr;
+      int res = ptr == p;
+      return res;
+    }
+  }
+  return 0;
 }
 
 
-void free(void* ptr) {
+void custom_free(void *ptr)
+{
+  if (!ptr)
+  {
+    return;
+  }
+  if (!is_valid_addr(ptr))
+  {
+    return;
+  }
+  meta_data block = get_block_addr(ptr);
+  if (block->free)
+  {
+    return;
+  }
+  block->free = 1;
+  if (block->prev && block->prev->free)
+  {
+    block = merge_blocks(block->prev);
+  }
 
+  if (block->next)
+  {
+    block = merge_blocks(block);
+  }
+  else
+  {
+    if (block->prev)
+    {
+      block->prev->next = NULL;
+    }
+    else
+    {
+      heap_head = NULL;
+    }
+    brk(block);
+  }
 }
 
-void *realloc(void *ptr, size_t size) {
- return NULL;
-}
-
-void *calloc(size_t nelem, size_t elsize) {
+void *custom_realloc(void *ptr, size_t size)
+{
   return NULL;
+}
 
+void *custom_calloc(size_t nelem, size_t elsize)
+{
+  return NULL;
 }
--- custom-alloc/custom_mem_management.h
+++ custom-alloc/custom_mem_management.h
@@ -3,8 +3,8 @@
 
 #include "custom_alloc.h"
 
-void free(void* ptr);
-void *realloc(void *ptr, size_t size);
-void *calloc(size_t nelem, size_t elsize);
+void custom_free(void* ptr);
+void *custom_realloc(void *ptr, size_t size);
+void *custom_calloc(size_t nelem, size_t elsize);
 
 #endif // !CUSTOM_MEM_MANAGMENT_H
