d8151b641fec428fb428e97bdf11c0c1a47baf88
jojo-prog <johannes16.braeuer@gmail.com>
2025-01-21 12:31:12 +0100

    feat: best fit algorithm

--- custom-alloc/best_fit.c
+++ custom-alloc/best_fit.c
@@ -2,4 +2,20 @@
 meta_data find_free_block(meta_data *prev, size_t size)
 {
    // implement best fit
+
+   meta_data current = heap_head;
+   meta_data best_fit = NULL;
+   size_t min_size = -1;
+
+   while (current)
+   {
+       if (current->free && current->size >= size && current->size < min_size)
+       {
+           best_fit = current;
+           min_size = current->size;
+           *prev = current;
+       }
+       current = current->next;
+   }
+
 }
