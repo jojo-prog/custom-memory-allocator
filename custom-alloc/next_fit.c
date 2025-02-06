5f334c2d0c109c322cc4245ce08f8bca56f02a44
jojo-prog <johannes16.braeuer@gmail.com>
2025-02-06 14:36:10 +0100

    delete:

--- custom-alloc/next_fit.c
+++
@@ -1,7 +0,0 @@
-#include "custom_alloc.h"
-
-
-
-
-//This function searches for a free memory block starting from the last allocated block (`last_allocated`) 
-//and continues the search in a circular manner through the linked list of memory blocks.
